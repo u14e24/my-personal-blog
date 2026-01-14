3fbe5272f24bbff55e0c480f9157aa7edbaf283a
Zoran Soptija <zoran.soptija@gmail.com>
2026-01-14 19:19:52 +0100

    Add correct db relationships in models/post.py and models/tag.py

--- backend/app/models/post.py
+++ backend/app/models/post.py
@@ -1,13 +1,15 @@
-from typing import List
+from typing import List, Optional
 from sqlmodel import SQLModel, Field, Relationship
-
+from .user import User
 from .post_tag import PostTag
 
 class Post(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     title: str
     content: str
+    user_id: int = Field(foreign_key="user.id")
+    user: Optional[User] = Relationship (back_populates="posts")
     tags: List["Tag"] = Relationship(
-        #back_populates="",
+        back_populates="posts",
         link_model = PostTag
     )
--- backend/app/models/tag.py
+++ backend/app/models/tag.py
@@ -1,5 +1,10 @@
 from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
+from typing import List, Optional
+from .post_tag import PostTag
 
 class Tag(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True, unique=True)
+
+    posts: List["Post"] = Relationship(back_populates="tags", link_model=PostTag)
