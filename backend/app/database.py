30cc576906d77f9ce2b2050904cf4687a181f616
Zoran Soptija <zoran.soptija@gmail.com>
2026-01-14 19:42:00 +0100

    Update db_name

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,9 @@
 from sqlmodel import SQLModel, create_engine, Session
+from models.post import Post
+from models.post_tag import PostTag
+from models.tag import Tag
 
-DATABASE_URL = "sqlite:///./blog.db"
+DATABASE_URL = "sqlite:///./blog_zsoptij.db"
 engine = create_engine(DATABASE_URL, echo=True)
 
 def get_session():
